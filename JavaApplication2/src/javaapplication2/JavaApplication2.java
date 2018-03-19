7abf8a634d5ead563f0e16009256b41dc5b2d23c
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2018-03-19 14:08:32 +0800

    mit komat kamit ayak paa

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaApplication2;
+
+/**
+ *
+ * @author BESTI
+ */
+public class JavaApplication2 {
+        //attribut
+    string NPM;
+    string nama;
+    double IPK;
+    char kelas;
+    char nilaiPB01;
+    boolean statusAktif;
+}
+
+//behavior
+void infoMhs (){
+    System.out.printIn("NPM     :"+NPM);
+    System.out.printIn("Nama    :"+nama);
+    System.out.printIn("IPK     :"+IPK);
+    System.out.printIn("kelas   :"+nilaiPB01);
+    System.out.printIn("Status  :"+statusAktif);
+}
+
+void IsiAbsen (){
+}
+
+void ubahNilai (char )
